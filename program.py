76bcfa387e5753f5167960447112f63889745796
filin <tima.dudich.97@mail.ru>
2021-08-03 20:10:00 +0300

    Now user choose the end number by himself

--- program.py
+++ program.py
@@ -1,7 +1,7 @@
-#The program count the sum of squares of numbers from 1 to 9
+#The program count the sum of squares of numbers from 1 to number chosen by user
 
 sum=0
-for i in range(1,10):
+for i in range(1,int(input())):
 	print(i*i, end='')
 	sum+=i
 print("The sum is:{}".format(sum))
